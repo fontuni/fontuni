4f4a35818615abe1823154c571cbf9d4c2f5060c
Sungsit Sawaiwan <sungsit.sawaiwan@gmail.com>
2014-04-25 14:34:29 +0700

    Add Pango markup test file

--- test/pango-view.sh
+++ test/pango-view.sh
@@ -1,17 +1,15 @@
-pango-view \
+pango-view test-markup.txt \
   --font="F0ntUni A 60px" \
   --header \
   --pixels --width=660 \
   --language=th_TH \
   --markup \
-  --text 'พี่ป๋ำฎูนู๋น้ำเป่าฝุ่นหญู่ก้นปี่ กตัญญู <span lang="pi-TH">ฐาตุํ ญาติํ ปุํปิํ</span>' \
   --output=pango-a.png
 
-pango-view \
+pango-view test-markup.txt \
   --font="F0ntUni B 60px" \
   --header \
   --pixels --width=660 \
   --language=th_TH \
   --markup \
-  --text 'พี่ป๋ำฎูนู๋น้ำเป่าฝุ่นหญู่ก้นปี่ กตัญญู <span lang="pi-TH">ฐาตุํ ญาติํ ปุํปิํ</span>' \
   --output=pango-b.png
